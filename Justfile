dbe65275317732a33d4568a3815ede859e8a779f
Nils Krüger <radsteve@radsteve.net>
2025-01-09 16:32:47 +0100

    feat: project scaffold

---
+++ Justfile
@@ -0,0 +1,8 @@
+build-release: build compress
+
+build:
+    RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features="optimize_for_size" --release
+
+compress:
+    upx --best --lzma target/release/glag
+    upx --best --lzma target/release/server
---
+++ glag/src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+use clap::Parser;
+
+#[derive(Debug, Clone, Parser)]
+/// Glagolica is a modern documentation tool designed to streamline development in every possible environment, fitting seamlessly into all languages and ecosystems with effortless pipeline integration and versatile customization options.
+/// Harnessing the elegance of Markdown, Glagolica transforms simplicity into brilliance, generating stunning web documentation that combines form with functionality.
+struct GlagCli {}
+
+fn main() -> io::Result<()> {
+    let cli = GlagCli::parse();
+    
+    println!("Hello from Glagolica! CLI: {cli:#?}");
+    
+    Ok(())
+}
